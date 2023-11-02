c8e43624bd62d9e3c6bf264095a682757fce00db
angelcsl1999 <angelcsl1999@hotmail.com>
2023-11-02 16:25:07 +0100

    tv_shows SVG FIXED

--- resources/views/components/t-v-show-card.blade.php
+++ resources/views/components/t-v-show-card.blade.php
@@ -5,7 +5,8 @@
     <div class="mt-2">
         <a href="{{ route('TVShows.show', $tvshow['id']) }}" class="text-lg mt-2 hover:text-gray-300">{{ $tvshow['name'] }}</a>
         <div class="flex items-center text-gray-400 text-sm mt-1">
-            <svg class="fill-current text-orange-500 w-4" viewBox="0 0 24 24"><g data-name="Layer 2"><path d="M17.56 21a1 1 0 01-.46-.11L12 18.22l-5.1 2.67a1 1 0 01-1.45-1.06l1-5.63-4.12-4a1 1 0 01-.25-1 1 1 0 01.81-.68l5.7-.83 2.51-5.13a1 1 0 011.8 0l2.54 5.12 5.7.83a1 1 0 01.81.68 1 1 0 01-.25 1l-4.12 4 1 5.63a1 1 0 01-.4 1 1 1 0 01-.62.18z" data-name="star"/></g></svg>
+            <svg class="fill-current text-orange-500 w-4" viewBox="0 0 24 24"><g data-name="Layer 2">
+                <path d="m19.448,23.309l-7.467-5.488-7.467,5.488,2.864-8.863L-.082,8.992h9.214L11.981.114l2.849,8.878h9.214l-7.46,5.453,2.864,8.863Zm-7.467-7.971l3.658,2.689-1.403-4.344,3.683-2.691h-4.548l-1.39-4.331v8.677Z">
             <span class="ml-1">{{ $tvshow['vote_average'] }}</span>
             <span class="mx-2">|</span>
             <span>{{ $tvshow['first_air_date'] }}</span>
