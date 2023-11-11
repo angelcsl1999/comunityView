742ddd9a1ddb4841d1e05710584f90cd8b9ac0e0
angelcsl1999 <angelcsl1999@hotmail.com>
2023-11-11 21:53:41 +0100

    implemented login with mailtrap

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,16 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+//verifie email uncomment this line
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+//use Illuminate\Foundation\Auth\User as Authenticatable;
+use Jenssegers\Mongodb\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+#implements or not mustVerifyemail
+class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -21,6 +24,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role'
     ];
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,28 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MoviesController;
 use App\Http\Controllers\ActorsController;
 use App\Http\Controllers\TVShowsController;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-/*
+
+
 Route::get('/', function () {
     return view('welcome');
 });
-*/
 
-//Get movies Controller->General
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
 
 Route::get('/movies',[MoviesController::class, 'index'])->name('movies.index');
 //Show an specific movie
@@ -39,9 +40,3 @@ Route::get('/actors/{actor}',[ActorsController::class, 'show'])->name('actors.sh
 
 Route::get('/TVShows',[TVShowsController::class, 'index'])->name('TVShows.index');
 Route::get('/TVShows/{tv}',[TVShowsController::class, 'show'])->name('TVShows.show');
-
-
-//dev rol only
-Route::get('/phpmyinfo', function () {
-    phpinfo(); 
-})->name('phpmyinfo');
