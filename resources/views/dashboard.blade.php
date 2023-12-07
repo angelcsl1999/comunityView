244f9d48f33f9b1d3039619c0b9df219074f47a0
angelcsl1999 <angelcsl1999@hotmail.com>
2023-12-07 16:24:08 +0100

    STORAGE RUTA FUNCIONAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,15 +12,8 @@
         I am not a writer...
         @endrole
     </div>  
-   
-        <img src=" {{storage_path('app\public\images\ComunityView-logos_white.png') }}" class="w-76 text-white" alt="FALLA">
-        <img src=" {{storage_path('app\public\images\file_example_JPG_100kB.jpg') }}" class="w-76 text-white" alt="FALLA5">
-        <br>
-
-
-        <img src="{{ base_path().'\public\storage\images\ComunityView-logos_white.png' }}" class="w-76 text-white" alt="FALLA2aaa">
-        <img src="{{ base_path().'\public\storage\images\file_example_JPG_100kB.jpg' }}" class="w-76 text-white" alt="FALLA2aaa">
-        <img src="{{ URL::asset('images\ComunityView-logos_white.png') }}" class="w-76 text-white"  alt="tag">
+    
+        <img src=" {{asset('storage/images/ComunityView-logos_white.png') }}" class="w-76 text-white" alt="FALLA">
         
 @endsection
 
