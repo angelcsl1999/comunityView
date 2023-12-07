f28dca00c71b5ff84f6d6db9e449a8ff457fe689
angelcsl1999 <angelcsl1999@hotmail.com>
2023-12-07 02:10:13 +0100

    final

--- database/migrations/2023_12_03_033529_create_roles_and_permissions.php
+++ database/migrations/2023_12_03_033529_create_roles_and_permissions.php
@@ -32,7 +32,6 @@ return new class extends Migration
 
         $premiumRole->givePermissionTo($watchPremiumPermission);
 
-        $user->givePermissionTo('edit articles');
     }
 
     /**
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,7 +12,16 @@
         I am not a writer...
         @endrole
     </div>  
-    
+   
+        <img src=" {{storage_path('app\public\images\ComunityView-logos_white.png') }}" class="w-76 text-white" alt="FALLA">
+        <img src=" {{storage_path('app\public\images\file_example_JPG_100kB.jpg') }}" class="w-76 text-white" alt="FALLA5">
+        <br>
+
+
+        <img src="{{ base_path().'\public\storage\images\ComunityView-logos_white.png' }}" class="w-76 text-white" alt="FALLA2aaa">
+        <img src="{{ base_path().'\public\storage\images\file_example_JPG_100kB.jpg' }}" class="w-76 text-white" alt="FALLA2aaa">
+        <img src="{{ URL::asset('images\ComunityView-logos_white.png') }}" class="w-76 text-white"  alt="tag">
+        
 @endsection
 
 
