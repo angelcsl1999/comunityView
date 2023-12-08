6063afc034d1be8781855de2b9309d61733827ec
angelcsl1999 <angelcsl1999@hotmail.com>
2023-12-08 04:10:58 +0100

    logo blade fixed

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,4 +1,4 @@
-<svg viewBox="0 0 316 316" class="text-gray-900">
-    
-</svg>
-<p class="text-slate-50"> Insertar logo</p>
+
+<a href="{{url('/')}}">
+    <img src=" {{asset('storage/images/ComunityView-logos_white.png') }}" class="object-fill h-48 w-70 text-white" alt="ComunityView">
+</a>
