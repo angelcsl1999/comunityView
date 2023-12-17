635724ff3bc156ebfffbf3572d377ec5de1b0780
angelcsl1999 <angelcsl1999@hotmail.com>
2023-12-17 19:10:10 +0100

    v

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,7 +3,7 @@
     <div class="container mx-auto px-4 pt-16">
         <div class="overflow-hidden shadow-sm sm:rounded-lg">
             <div class="p-6 text-gray-100">
-                <p>Logueado correctamente</p>   
+                <p class="text-white">Logueado correctamente</p>   
             </div>
             <button>
                 <button type="button" class="bg-orange-500 text-white active:bg-orange-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" onclick="window.location='{{ url("/") }}'">Inicio</button>
